f79a60438afeb9e24b81b67d9984d9ff429b2313
Didi Hoffmann <didi@ribalba.de>
2023-10-09 14:52:23 +0200

    Adds the column server

--- server.py
+++ server.py
@@ -9,10 +9,9 @@ from sqlalchemy import  select, func, Table, MetaData, Column, Integer, String, 
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.dialects.postgresql import JSONB
 from fastapi.responses import StreamingResponse
 
-DATABASE_URL = os.getenv("DATABASE_URL", 'postgresql+asyncpg://user:password@db/database_name')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql+asyncpg://user:password@db/database_name')
 
 app = FastAPI()
 
@@ -21,8 +20,17 @@ async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession
 
 metadata = MetaData()
 data_table = Table('data', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('data', JSONB)
+        Column('id', Integer, primary_key=True),
+        Column('time', BigInteger),
+        Column('node', Integer),
+        Column('combined_power', Float),
+        Column('cpu_energy', Float),
+        Column('gpu_energy', Float),
+        Column('ane_energy', Float),
+        Column('time_delta', Integer),
+        Column('screen_energy', Float),
+        Column('project', String),
+        Column('type', String),
 )
 
 @app.on_event("startup")
@@ -52,14 +60,25 @@ class Item(BaseModel):
 @app.post("/save")
 async def create_item(item: Item):
     async with async_session() as session:
-        stmt = insert(data_table).values(data=item.dict())
+        stmt = insert(data_table).values(
+            time=item.time,
+            node=item.node,
+            combined_power=item.combined_power,
+            cpu_energy=item.cpu_energy,
+            gpu_energy=item.gpu_energy,
+            ane_energy=item.ane_energy,
+            time_delta=item.time_delta,
+            screen_energy=item.screen_energy,
+            project=item.project,
+            type=item.type
+            )
         await session.execute(stmt)
         await session.commit()
 
 @app.get("/last_time/{node_id}")
 async def get_max_time(node_id: int):
     async with async_session() as session:
-        stmt = select(func.max(data_table.c.data['time'].cast(BigInteger))).where(data_table.c.data['node'].cast(Integer) == node_id)
+        stmt = select(func.max(data_table.c.time)).where(data_table.c.node == node_id)
         result = await session.execute(stmt)
         max_time = result.scalar()
         if max_time is None:
@@ -71,10 +90,10 @@ async def get_max_time(node_id: int):
 async def get_project_badge(project: str):
     async with async_session() as session:
         stmt = select(
-            func.sum(data_table.c.data['cpu_energy'].cast(Float)),
-            func.sum(data_table.c.data['gpu_energy'].cast(Float)),
-            func.sum(data_table.c.data['ane_energy'].cast(Float))
-        ).where(data_table.c.data['project'].astext == project)
+            func.sum(data_table.c.cpu_energy),
+            func.sum(data_table.c.gpu_energy),
+            func.sum(data_table.c.ane_energy)
+        ).where(data_table.c.project == project)
         result = await session.execute(stmt)
         cpu_energy, gpu_energy, ane_energy = result.one()
 
